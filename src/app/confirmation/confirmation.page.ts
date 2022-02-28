30aa3cd24f488ef692ff8a769d69f6466917582f
omerfarukbasekin <omerfarukbasekin@hotmail.com>
2022-02-28 15:16:42 +0300

    mobile

--- src/app/confirmation/confirmation.page.ts
+++ src/app/confirmation/confirmation.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirmation',
@@ -7,11 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmationPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
   confirmationFunc(){
     console.log("Congratulations");
+    
+    this.router.navigate(["/home"]);
   }
 }
