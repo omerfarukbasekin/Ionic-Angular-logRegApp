6f8420031a720f6f4f917c1361a3554827e6318f
omerfarukbasekin <omerfarukbasekin@hotmail.com>
2022-03-04 11:39:56 +0300

    ionic login upgrades

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import { MyService } from '../sign-up/my.service';
+import { toastController } from '@ionic/core';
+
 
 @Component({
   selector: 'app-login',
@@ -11,27 +14,73 @@ import { FormBuilder, FormControl, FormGroup} from '@angular/forms';
 export class LoginPage implements OnInit {
 
   private formLogData: FormGroup;
+  /* public data = {}; */
 
-  constructor(private router : Router) { }
+  constructor(private router : Router) {
+/*     
+      CONSTRUCTOR => ,private myService: MyService
+      this.myService.myMethod$.subscribe((data) => {
+      this.data = data; // And he have data here too!
+  }
+); */
+   }
 
   ngOnInit() {
     this.formLogData = new FormGroup({
       mail: new FormControl(),
       password: new FormControl(),
-      rememberBox: new FormControl()      
-    });
+      rememberBox : new FormControl()      
+    }); 
   }
+   Cmail:String = 'innova@mail.com';
+   Cpas:String = '123456';
 
   onSubmit(){
+    if(this.formLogData.value.mail == this.Cmail){
+      if(this.formLogData.value.password == this.Cpas){
+        this.router.navigate(["/confirmation"]);
+      }
+      else{
+        console.log("Your password is not match!");
+        this.handleWrongPass();
+      }
+    }
+    else{
+      console.log("E-mail adress is wrong!");
+      this.handleWrongMail();
+    }
     console.log(this.formLogData.value);
-    this.router.navigate(["/confirmation"]);
   }
+
   forgotPas(){
     console.log("new password function");
   }
   sentSignPage(){
     console.log("sending to sign up page");
     this.router.navigate(["/sign-up"]);
+
   }
 
+
+  async handleWrongMail() {
+    const toast = await toastController.create({
+      color: 'dark',
+      duration: 2000,
+      message: 'E-mail is not correct!',
+      keyboardClose: true
+    });
+
+    await toast.present();
+  }
+  async handleWrongPass() {
+    const toast = await toastController.create({
+      color: 'dark',
+      duration: 2000,
+      message: 'Password is not correct!',
+      keyboardClose: true
+    });
+
+    await toast.present();
+  }
+                                              
 }
--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MyService } from './my.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -12,7 +13,13 @@ export class SignUpPage implements OnInit {
 
   private formData: FormGroup;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+   
+/*     Try sent value between components/pages 
+    constructor => , private myService: MyService
+    this.myService.myMethod(this.data); */
+    
+   }
 
   ngOnInit() {
     this.formData = new FormGroup({
@@ -21,12 +28,16 @@ export class SignUpPage implements OnInit {
       agreementBox: new FormControl()
     });
 
+    
 
   }
+  data: String ="f";
 
   onSubmit(){
     console.log(this.formData.value);
-    this.router.navigate(["/confirmation"]);
+/*     Try sent value between components/pages
+    this.data = this.formData.value.mail; */
+    this.router.navigate(["/login"]);
   }
 
   sentLoginPage(){
